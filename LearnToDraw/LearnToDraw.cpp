4b5618058ddc39f366a294164ec8a6bbcf3034bd
Devyn <freakanature32@hotmail.com>
2022-03-16 22:15:51 -0700

    Add project files.

---
+++ LearnToDraw/LearnToDraw.cpp
@@ -0,0 +1,12 @@
+﻿// LearnToDraw.cpp : Defines the entry point for the application.
+//
+
+#include "LearnToDraw.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
